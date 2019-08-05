48325a17230dac1138049a0ead81df953bf5334d
BuildTools <unconfigured@null.spigotmc.org>
2019-08-05 16:03:29 -0400

    toot toot !

--- project1.py
+++ project1.py
@@ -1,6 +1,29 @@
 def main():
-     print ("Hello World")
+    print("Hello World")
 
 
 if __name__ == "__main__":
     main()
+
+#name = input("What is your name? ")
+#print ("hello " + name)
+
+#x = 10
+#print(x)
+
+my_list = []
+for i in range(11):
+    my_list.append(i)
+print(my_list)
+
+x = int(input("enter a number"))
+if x < 10:
+    print ("less than 10")
+if x > 10:
+    print ("greater than 10")
+
+if x % 2 == 0:
+    print ("even number")
+else:
+    print ("odd number")
+
